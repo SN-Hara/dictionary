bfca28dd013aff4bb882c46ee2c33b26ae7474b9
SN-Hara <156044755+SN-Hara@users.noreply.github.com>
2026-09-12 16:37:30 +0900

    辞書を更新

---
+++ data.js
@@ -0,0 +1,296 @@
+window.GAME_DATA = {
+  "dictionary": [
+    {
+      "heading": "ああ",
+      "notation": "ああ",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "ああ-001"
+    },
+    {
+      "heading": "あい",
+      "notation": "あい",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あい-002"
+    },
+    {
+      "heading": "あう",
+      "notation": "あう",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あう-003"
+    },
+    {
+      "heading": "あえ",
+      "notation": "あえ",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あえ-004"
+    },
+    {
+      "heading": "あお",
+      "notation": "あお",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あお-005"
+    },
+    {
+      "heading": "あか",
+      "notation": "あか",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あか-006"
+    },
+    {
+      "heading": "あき",
+      "notation": "あき",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あき-007"
+    },
+    {
+      "heading": "あく",
+      "notation": "あく",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あく-008"
+    },
+    {
+      "heading": "あけ",
+      "notation": "あけ",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あけ-009"
+    },
+    {
+      "heading": "あこ",
+      "notation": "あこ",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あこ-010"
+    },
+    {
+      "heading": "あさ",
+      "notation": "あさ",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あさ-011"
+    },
+    {
+      "heading": "あし",
+      "notation": "あし",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あし-012"
+    },
+    {
+      "heading": "あす",
+      "notation": "あす",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あす-013"
+    },
+    {
+      "heading": "あせ",
+      "notation": "あせ",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あせ-014"
+    },
+    {
+      "heading": "あそ",
+      "notation": "あそ",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あそ-015"
+    },
+    {
+      "heading": "あた",
+      "notation": "あた",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あた-016"
+    },
+    {
+      "heading": "あち",
+      "notation": "あち",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あち-017"
+    },
+    {
+      "heading": "あつ",
+      "notation": "あつ",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あつ-018"
+    },
+    {
+      "heading": "あて",
+      "notation": "あて",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あて-019"
+    },
+    {
+      "heading": "あと",
+      "notation": "あと",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "あと-020"
+    },
+    {
+      "heading": "うそ",
+      "notation": "嘘",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "Lie",
+      "id": "うそ-021"
+    },
+    {
+      "heading": "しんごう",
+      "notation": "信号",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "しん-022"
+    },
+    {
+      "heading": "しんじつ",
+      "notation": "真実",
+      "description": "嘘の奥に隠れているもの。卒業式の日に明かされたこと。",
+      "keywords": [
+        "嘘",
+        "卒業式"
+      ],
+      "flag": "Truth",
+      "id": "しん-023"
+    },
+    {
+      "heading": "しんぞう",
+      "notation": "心臓",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "しん-024"
+    },
+    {
+      "heading": "しんぶんきしゃ",
+      "notation": "新聞記者",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "しん-025"
+    },
+    {
+      "heading": "しんや",
+      "notation": "深夜",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "しん-026"
+    },
+    {
+      "heading": "しんゆう",
+      "notation": "親友",
+      "description": "秘密を共有する相手。薫のこと。",
+      "keywords": [
+        "秘密"
+      ],
+      "flag": "BestFriend",
+      "id": "しん-027"
+    },
+    {
+      "heading": "しんろ",
+      "notation": "進路",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "しん-028"
+    },
+    {
+      "heading": "しんわ",
+      "notation": "神話",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "しん-029"
+    },
+    {
+      "heading": "そつぎょうしき",
+      "notation": "卒業式",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "GraduationCeremony",
+      "id": "そつ-030"
+    },
+    {
+      "heading": "ひみつ",
+      "notation": "秘密",
+      "description": "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ",
+      "keywords": [],
+      "flag": "",
+      "id": "ひみ-031"
+    }
+  ],
+  "events": [
+    {
+      "id": "Prologue",
+      "type": "flag_count",
+      "detail": "0"
+    },
+    {
+      "id": "BestFriend",
+      "type": "flag",
+      "detail": "BestFriend"
+    },
+    {
+      "id": "Event_001",
+      "type": "flag_count",
+      "detail": "2"
+    }
+  ],
+  "scripts": [
+    {
+      "eventId": "Prologue",
+      "order": 1,
+      "text": "誰か教えて……\n私のすべてを──"
+    },
+    {
+      "eventId": "Prologue",
+      "order": 2,
+      "text": "まずは「しん」で検索しましょう"
+    },
+    {
+      "eventId": "BestFriend",
+      "order": 1,
+      "text": "「親友」の記憶を修復すると\nこの文章が表示されます"
+    },
+    {
+      "eventId": "Event_001",
+      "order": 1,
+      "text": "記憶を二つ以上修復すると\nこの文章が表示されます"
+    }
+  ]
+};
